57be8393e5db57c7be29b6f17f762453cc2c03d9
M-Pestka <michal.pestka1@gmail.com>
2020-01-27 20:17:21 +0100

    komentarze

--- ant_colony.py
+++ ant_colony.py
@@ -10,6 +10,7 @@ class ant_colony:
 
 
     def _sim_single_ant(self):
+        # symulacja pojedynczej mrowki
         path = self._randomize_path()
         path_len = self.calculate_path_len(path)
         for src, dst in zip(path[:-1], path[1:]):
@@ -26,16 +27,22 @@ class ant_colony:
         return converged
 
     def _randomize_path(self):
+        # zwraca wylosowaną scieżkę. Losowanie odbywa się z uwzględnieniem feromonów pozostawionych przez
+        # poprzednie mrówki
         raise Exception('Not implemented')
 
     def reset(self):
+        # resetuje solver i czyści graf
         raise Exception('Not implemented')
 
     def _make_intersection_decision(self):
+        # funkcja pomocnicza do decydowania w którą stronę mrowka powinna się udac
         raise Exception('Not implemented')
 
     def check_conversion(self):
+        # sprawdza warunek zbiegnięcia algorytmu
         raise Exception('Not implemented')
 
     def calculate_path_len(self, path):
+        # liczy długość scieżki
         raise Exception('Not implemented')
